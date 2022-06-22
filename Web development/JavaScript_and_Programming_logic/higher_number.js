48aa67356ff49fdc671aa57991b9eacab4c81ac4
Lais Omena <laisf.omena@gmail.com>
2022-06-22 13:22:40 +0100

    Exercise 2 :rocket:

---
+++ Web development/JavaScript_and_Programming_logic/higher_number.js
@@ -0,0 +1,11 @@
+//Faça um programa que retorne o maior de dois números. 
+//Defina no começo do programa duas constantes com os valores que serão comparados.
+
+const num1 = 4;
+const num2 = 9;
+
+if(num1 < num2){
+    console.log('O maior é ' + num2);
+} else if (num1 > num2){
+    console.log('O maior é ' + num1);
+}

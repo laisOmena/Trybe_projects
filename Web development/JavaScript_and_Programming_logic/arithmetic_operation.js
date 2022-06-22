1261ddb2975af1a602eee5761813d1fab8129593
Lais Omena <laisf.omena@gmail.com>
2022-06-22 13:15:44 +0100

    Creating exercises :rocket:

---
+++ Web development/JavaScript_and_Programming_logic/arithmetic_operation.js
@@ -0,0 +1,26 @@
+let a = 2;
+let b = 5;
+
+//Adição
+let soma = a + b;
+console.log(soma);
+
+//subtração
+
+let subtracao = b - a;
+console.log(subtracao);
+
+//Multiplicação
+
+let mult = a * b;
+console.log(mult);
+
+//Divisão
+
+let divisao = b / a;
+console.log(divisao);
+
+//Modúlo
+
+let resto = b % a;
+console.log(resto);

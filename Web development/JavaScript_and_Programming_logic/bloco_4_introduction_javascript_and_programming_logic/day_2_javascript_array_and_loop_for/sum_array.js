83d887de00baea94787136ccc650e26a0704adb2
Lais Omena <laisf.omena@gmail.com>
2022-06-22 20:06:37 +0100

    Exercise 2: somando elementos do array :rocket:

---
+++ Web development/JavaScript_and_Programming_logic/bloco_4_introduction_javascript_and_programming_logic/day_2_javascript_array_and_loop_for/sum_array.js
@@ -0,0 +1,9 @@
+/* Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+ */
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sumNumbers = 0;
+for(let i = 0; i < numbers.length; i++){
+    sumNumbers += numbers[i];
+}
+console.log(sumNumbers);

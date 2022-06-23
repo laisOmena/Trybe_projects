12c27b08bc3a294a3be12c9c3ac3ad91516410df
Lais Omena <laisf.omena@gmail.com>
2022-06-23 15:09:21 +0100

    Exercise bonus :rocket:

---
+++ Web development/JavaScript_and_Programming_logic/bloco_4_introduction_javascript_and_programming_logic/day_2_javascript_array_and_loop_for/bonus_decreasing_array.js
@@ -0,0 +1,15 @@
+/* Ordene o array numbers em ordem decrescente e imprima seus valores; */
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for(let i = 0; i < numbers.length; i += 1){
+    for (let j = 0; j < i; j += 1){
+        if (numbers[i] > numbers[j]){
+            let position = numbers[j];
+            numbers[j] = numbers[i];
+            numbers[i] = position;
+        }
+    }
+}
+
+console.log(numbers);

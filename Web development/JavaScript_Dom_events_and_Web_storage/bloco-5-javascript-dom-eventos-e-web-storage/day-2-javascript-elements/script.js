e5c9736e8a7d928f3540214053dbdbe9fc25c77f
Lais Omena <laisf.omena@gmail.com>
2022-06-30 15:25:16 +0100

    Exercises day 2 :rocket:

---
+++ Web development/JavaScript_Dom_events_and_Web_storage/bloco-5-javascript-dom-eventos-e-web-storage/day-2-javascript-elements/script.js
@@ -0,0 +1,22 @@
+let ingredientItems = [
+    '500g de feijão carioquinha cozido',
+    '200g de toucinho',
+    '1 concha de óleo',
+    '1 cebola média picada',
+    '4 dentes de alho',
+    '5 ovos',
+    '1 colher de sopa de sal com alho',
+    'Cheiro verde a gosto',
+    '200g de farinha de mandioca'
+];
+
+let ingredientList = document.querySelector('.ingredients-list');
+
+for(let i = 0; i < ingredientItems.length; i += 1){ //Percorrendo o array ingredientItems
+    let ingredient = ingredientItems[i];//Adicionando cada elemento na variável ingredient
+
+    let ingredientListItem = document.createElement('li');//Criando o elemento li na nova variável
+    ingredientListItem.innerText = ingredient;//Adicionando cada item de ingredient em texto na variavel ingredientListItem
+
+    ingredientList.appendChild(ingredientListItem);//Inserindo a variavel ingredientListItem como filha de ingredientList
+}

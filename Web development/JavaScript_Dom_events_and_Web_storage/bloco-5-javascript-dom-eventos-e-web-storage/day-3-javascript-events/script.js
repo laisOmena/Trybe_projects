795ef5b2ea5a5df893b9e16314b342ede22e0501
Lais Omena <laisf.omena@gmail.com>
2022-07-02 17:49:18 +0100

    Resolvendo conflitos :rocket:

--- Web development/JavaScript_Dom_events_and_Web_storage/bloco-5-javascript-dom-eventos-e-web-storage/day-3-javascript-events/script.js
+++ Web development/JavaScript_Dom_events_and_Web_storage/bloco-5-javascript-dom-eventos-e-web-storage/day-3-javascript-events/script.js
@@ -10,7 +10,6 @@ function createDaysOfTheWeek() {
         weekDaysList.appendChild(dayListItem);
     };
 };
-<<<<<<< HEAD
 
 createDaysOfTheWeek();
 
@@ -27,15 +26,6 @@ function createDays() {
 
         let daysListItem = document.createElement('li');
         daysListItem.classList.add('day')
-=======
-  
-createDaysOfTheWeek();
-  
-  // Escreva seu código abaixo.
-function createDays() {
-  let decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
-  let daysList = document.getElementById('days');
->>>>>>> main
 
         if ((day === 24) || (day === 25) || (day === 31)) {
             daysListItem.classList.add('holiday');
@@ -104,12 +94,6 @@ function buttonCreate(element) {
   divButton.appendChild(btn);
 }
 
-    daysListItem.innerText = day;
-    daysList.appendChild(daysListItem);
-  }
-}
-
-createDays();
 /* Implemente uma função que crie dinamicamente um botão com o nome "Feriados".
 Sua função deve receber um parâmetro com a string 'Feriados'
 Adicione a este botão a ID "btn-holiday"

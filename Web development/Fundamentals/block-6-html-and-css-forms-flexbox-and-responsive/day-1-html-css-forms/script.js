82ee8dff4e50ecc0c75cb351d3f9d22981c9f317
Lais Omena <laisf.omena@gmail.com>
2022-07-07 13:51:53 +0100

    Question 2 :rocket:

--- Web development/Fundamentals/block-6-html-and-css-forms-flexbox-and-responsive/day-1-html-css-forms/script.js
+++ Web development/Fundamentals/block-6-html-and-css-forms-flexbox-and-responsive/day-1-html-css-forms/script.js
@@ -3,7 +3,7 @@ let btnEnviar = document.getElementById('input-enviar');
 function getElement(event) {
     event.preventDefault();
     let element = btnEnviar.value;
-    return element;
+    console.log('okay');
 }
 
 btnEnviar.addEventListener('click', getElement);

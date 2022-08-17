a1922f6786aa14ea41921b2bf2fa7c64e9352fc6
Lais Omena <laisf.omena@gmail.com>
2022-08-17 11:10:42 +0100

    Create component list and add list :rocket:

--- Front-end/Introduction of react/day-1-hello-world-in-react/exercise-10.1/src/App.js
+++ Front-end/Introduction of react/day-1-hello-world-in-react/exercise-10.1/src/App.js
@@ -1,11 +1,22 @@
-import React, { Component } from 'react';
+import React, { Fragment } from 'react';
 import './App.css';
-import List from './components/list';
+
+const animes = ['One Piece', 'Hunter x Hunter', 'Fairy Tail', 'Naruto', 'Bleach'];
+const Task = (props) => {
+  return (
+    <Fragment>
+      <h1>list</h1>
+      <ul>
+        {props.value.map(task => <li>{task}</li>)}
+      </ul>
+    </Fragment>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <List/>
+      <Task value={animes} />
     </div>
   );
 }
